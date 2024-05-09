20a183f6818363b099a993b9f45f1bdaee8fac4f
athenalobo <athenamlobo@gmail.com>
2024-05-09 20:13:39 +0530

    simon game initial

---
+++ Simon Game Challenge Starting Files/index.js
@@ -0,0 +1,14 @@
+var gamePattern=[];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random()*4);
+    return randomNumber;
+};
+
+var randomChosenColour = buttonColours[nextSequence()];
+
+gamePattern.push(randomChosenColour);
+
+$(randomChosenColour)
